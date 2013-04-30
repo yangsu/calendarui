abd8824750effd2da3433854a71f10f889cf2055
Yang Su <suyang1+services@gmail.com>
2013-04-30 15:06:38 -0700

    added data

---
+++ app/scripts/data.js
@@ -0,0 +1,88 @@
+var data = {};
+data.columns = [
+    {
+        name:'Name',
+        type:'text',
+        id:75723
+    },
+    {
+        name:'Cell Number',
+        type:'phoneNumber',
+        id:113509
+    },
+    {
+        name:'Profile Photo',
+        type:'picture',
+        id:9299
+    },
+    {
+        name:'Interview Start Time',
+        type:'date',
+        id:48929
+    },
+    {
+        name:'Interview End Time',
+        type:'date',
+        id:52213
+    },
+    {
+        name:'Birthday',
+        type:'date',
+        id:914737
+    },
+    {
+        name:'Application Submission Date',
+        type:'date',
+        id:13221
+    },
+];
+data.rows = [
+    {
+        75723:'John Doe',
+        113509:'3214231175',
+        9299:{
+            large:'https://graph.facebook.com/howietl/picture?type=large',
+            small:'https://graph.facebook.com/howietl/picture?type=small'
+        },
+        48929:'5/4/13 3:00PM',
+        52213:'5/4/13 5:00PM',
+        914737:'4/3/77',
+        13221:'4/29/13'
+    },
+    {
+        75723:'Jane Smith',
+        113509:'9791023214',
+        9299:{
+            large:'https://graph.facebook.com/1193/picture?type=large',
+            small:'https://graph.facebook.com/1193/picture?type=small'
+        },
+        48929:'5/4/13 2:30PM',
+        52213:'5/4/13 3:30PM',
+        914737:'2/1/84',
+        13221:'4/13/13'
+    },
+    {
+        75723:'Mary Bazker',
+        113509:'9921012954',
+        9299:{
+            large:'https://graph.facebook.com/1467/picture?type=large',
+            small:'https://graph.facebook.com/1467/picture?type=small'
+        },
+        48929:'5/7/13 1PM',
+        52213:'5/7/13 8PM',
+        914737:'4/2/91',
+        13221:'3/29/13'
+    },
+    {
+        75723:'Blake Bruno',
+        113509:'8372125313',
+        9299:{
+            large:'https://graph.facebook.com/3500/picture?type=large',
+            small:'https://graph.facebook.com/3500/picture?type=small'
+        },
+        48929:'5/15/13 2PM',
+        52213:'5/15/13 2:30PM',
+        914737:'3/5/87',
+        13221:'4/30/13'
+    }
+];
